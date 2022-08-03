fa1d49413a6a1da1955f5216cf9f5edf11cffce0
idfurnl <mr.erik770@mail.ru>
2022-08-04 01:30:50 +0500

    how do i suppose start w/o design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Footer />
         <h1>im ready</h1>
-      </header>
     </div>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from "./Footer.module.scss"
+
+const Footer = () => {
+    return (
+        <div className={classes.footer}>
+            <h1>hello</h1>
+            <h2>end</h2>
+        </div>
+    );
+};
+
+export default Footer;
