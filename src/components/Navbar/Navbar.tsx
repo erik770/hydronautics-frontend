7c741079bdc4897f8e724f8eff28eb35f96994c7
idfurnl <mr.erik770@mail.ru>
2022-08-08 00:22:41 +0500

    navbar done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import classes from "./Navbar.module.scss"
 import {MenuItems} from "../../const/MenuItems";
 import logo from '../../img/logo_with_text.png';
 
 export const Navbar = () => {
+    useEffect(() => {
+        const scriptTag = document.createElement('script');
+
+        scriptTag.src = "https://kit.fontawesome.com/99b107e3e1.js";
+        scriptTag.async = true;
+
+        document.body.appendChild(scriptTag);
+        return () => {
+            document.body.removeChild(scriptTag);
+        }
+    }, []);
+
+    const [menuActive, setMenuActive] = useState(false)
+    const menuOpen = () => setMenuActive(currentMenuActiveState => !currentMenuActiveState)
     return (
         <nav className={classes.navbar}>
             <h1 className={classes.navbar__logo}> <img src={logo}></img></h1>
+            <div className={classes['navbar__menu-icon']} onClick={menuOpen}>
+                <i className={menuActive ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
             <ul className={classes.navbar__items}>
                 {
                  MenuItems.map((item) => <li  key={item.title}> <a className={classes.navbar__item} href={item.url}>{item.title}</a></li>)
